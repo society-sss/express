78b9c360807d9b46ca2e75527067193942dca935
JalalovElzar <qwwgcc491@gmail.com>
2025-03-31 20:38:50 +0600

    Fixed dependencies

--- server/server.js
+++ server/server.js
@@ -137,5 +137,5 @@ app.patch('/api/delivery/:id', (req, res) => {
 
 
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Сервер запущен на порту ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
