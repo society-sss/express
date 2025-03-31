9c42b4eeb15ac63af729e7e5c93edd684ede45fc
JalalovElzar <qwwgcc491@gmail.com>
2025-03-31 20:12:06 +0600

    сделал маленькое изменение на server.js

--- server/server.js
+++ server/server.js
@@ -137,6 +137,5 @@ app.patch('/api/delivery/:id', (req, res) => {
 
 
 
-
-// const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`✅ Сервер запущен на порту 5000`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Сервер запущен на порту ${PORT}`))
